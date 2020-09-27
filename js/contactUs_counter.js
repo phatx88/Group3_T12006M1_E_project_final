f537455e2e31717f548c096ef654fd80afb3e2ad
Phat X Luong <66936482+phatx88@users.noreply.github.com>
2020-09-27 22:53:35 +0700

    redo counter algo

--- js/contactUs_counter.js
+++ js/contactUs_counter.js
@@ -1,30 +1,4 @@
-$(function () {
-    // $('.counter').each(function() {
-    //     var $this = $(this),
-    //         countTo = $this.attr('data-count');
-        
-    //     $({ countNum: $this.text()}).animate({
-    //       countNum: countTo
-    //     },
-      
-    //     {
-      
-    //       duration: 8000,
-    //       easing:'linear',
-    //       step: function() {
-    //         $this.text(Math.floor(this.countNum));
-    //       },
-    //       complete: function() {
-    //         $this.text(this.countNum);
-    //         //alert('finished');
-    //       }
-      
-    //     });  
-        
-        
-      
-    //   });
-
+$(function () {    
     $(window).scroll(countAnimation);
 	var viewed = false;
 
@@ -41,17 +15,35 @@ $(function () {
 	function countAnimation() {
 	  if (isScrolledIntoView($(".counter-wrapper")) && !viewed) {
 	      viewed = true;
-	      $('.counter-wrapper .counter').each(function () {
-	      $(this).prop('data-count',0).animate({
-	          Counter: $(this).text()
-	      }, {
-	          duration: 10000,
-	          easing: 'swing',
-	          step: function (now) {
-	              $(this).text(Math.ceil(now));
-	          }
-	      });
-	    });
+	    //   $('.counter-wrapper .counter').each(function () {
+	    //   $(this).prop('data-count',0).animate({
+	    //       Counter: $(this).text()
+	    //   }, {
+	    //       duration: 10000,
+	    //       easing: 'swing',
+	    //       step: function (now) {
+	    //           $(this).text(Math.ceil(now));
+	    //       }
+	    //   });
+		// });
+		$('.counter').each(function() {
+			var $this = $(this),
+				countTo = $this.attr('data-count');
+			
+			$({ countNum: $this.text()}).animate({
+			  countNum: countTo
+			}, {
+			  duration: 8000,
+			  easing:'swing',
+			  step: function() {
+				$this.text(Math.floor(this.countNum));
+			  },
+			  complete: function() {
+				$this.text(this.countNum);
+				//alert('finished');
+			  }     
+			});              
+		  });
 	  }
 	}
 });
